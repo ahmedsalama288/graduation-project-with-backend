78044ac9b422856cc3da47b3008df26146a57e3c
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-03 16:51:04 +0200

    fix footer style

--- src/app/ui/root-layout/footer-section/footer-copy-write.tsx
+++ src/app/ui/root-layout/footer-section/footer-copy-write.tsx
@@ -4,7 +4,7 @@ export default function FooterCopyWrite() {
       <div dir="ltr" className=" container mx-auto p-4 bg-[#013e60] rounded-lg">
         <span className="flex flex-col sm:flex-row sm:gap-2 justify-center items-center">
           <span className=" flex justify-center items-center">
-            <span className="pt-[2px] h-fit text-xl mr-[2px]">&copy;</span>
+            <span className="pt-[4px] h-fit text-xl mr-[2px]">&copy;</span>
             <span>2024</span>
           </span>
           <span className=" text-center">
