b71b3c9852385adbb46ea37bfda7979244626abb
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-04 19:37:28 +0200

    update member-photo size

--- src/app/ui/about-us/institute-management/member-photo.tsx
+++ src/app/ui/about-us/institute-management/member-photo.tsx
@@ -9,11 +9,12 @@ export default function MemberPhoto({ imageSrc, alt }: props) {
   return (
     <div>
       <Image
-        className=" w-[200px] rounded-lg"
+        className=" w-[200px] h-[247.321px] rounded-lg"
         src={imageSrc}
         quality={100}
-        width={10000}
-        height={10000}
+        width={3000}
+        height={3000}
+        priority={true}
         alt={alt}
       />
     </div>
