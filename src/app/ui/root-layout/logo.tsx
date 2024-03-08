5432783447f2068e1fd1802672716975b0ea1264
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-08 18:27:53 +0200

    remove the className prop from the logo component

--- src/app/ui/root-layout/logo.tsx
+++ src/app/ui/root-layout/logo.tsx
@@ -2,15 +2,11 @@ import Image from "next/image";
 import logo from "@/../../public/logo.png";
 import Link from "next/link";
 
-interface props {
-  className?: string;
-}
-
-export default function Logo({ className }: props) {
+export default function Logo() {
   return (
     <Link href="/" className="drop-shadow-lg inline-block">
       <Image
-        className={`${className} w-[70px] h-[70px] min-w-[70px] max-w-[70px] drop-shadow-md`}
+        className="w-[70px] h-[70px] min-w-[70px] max-w-[70px] drop-shadow-md"
         src={logo}
         priority
         sizes=""
