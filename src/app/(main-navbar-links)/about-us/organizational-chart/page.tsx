8d6c7ee268ca8c8dda4ebe16dde30d192c65bd49
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-09 00:02:29 +0200

    add new organizational-chart page

--- src/app/(main-navbar-links)/about-us/organizational-chart/page.tsx
+++ src/app/(main-navbar-links)/about-us/organizational-chart/page.tsx
@@ -1,3 +1,25 @@
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
+import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
+import organizationalChartImage from "@/../../public/about-us/organizational-chart/organizational-chart.jpeg";
+import Image from "next/image";
+
 export default function Page() {
-  return <h1>الهيكل التنظيمي</h1>;
+  return (
+    <section>
+      <MainPageHeader text="الهيكل التنظيمي" />
+      <PageContentContainer className=" flex justify-center items-center">
+        <ContentWrapper className="mt-0 w-fit lg:h-[600px] flex justify-center items-center">
+            <Image
+              width={10000}
+              height={10000}
+              priority={false}
+              className="rounded-sm sm:rounded-md w-auto h-full "
+              src={organizationalChartImage}
+              alt="الهيكل التنظيمي للمعهد"
+            />
+        </ContentWrapper>
+      </PageContentContainer>
+    </section>
+  );
 }
