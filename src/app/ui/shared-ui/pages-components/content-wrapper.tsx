82f8bc51e17d9891b0877595db851e1692e59403
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-09 00:02:04 +0200

    edit component to accept className prop

--- src/app/ui/shared-ui/pages-components/content-wrapper.tsx
+++ src/app/ui/shared-ui/pages-components/content-wrapper.tsx
@@ -1,10 +1,14 @@
 export default function ContentWrapper({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <div className="bg-slate-500 rounded-md p-3 sm:p-4 mt-4 flex flex-col gap-4">
+    <div
+      className={`${className} bg-slate-500 rounded-md p-3 sm:p-4 mt-4 flex flex-col gap-4`}
+    >
       {children}
     </div>
   );
--- src/app/ui/shared-ui/pages-components/page-content-container.tsx
+++ src/app/ui/shared-ui/pages-components/page-content-container.tsx
@@ -1,10 +1,14 @@
 export default function PageContentContainer({
   children,
+  className,
 }: {
   children: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <div className=" container mx-auto p-4 sm:p-8 bg-slate-400 mt-8 rounded-lg">
+    <div
+      className={`${className} container mx-auto p-4 sm:p-8 bg-slate-400 mt-8 rounded-lg`}
+    >
       {children}
     </div>
   );
