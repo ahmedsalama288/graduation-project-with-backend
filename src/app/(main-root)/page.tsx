89cd39ab76f58de1352d2bb49fdb01a6eb28c0fe
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-26 04:56:29 +0200

    add AnimatedSection component to the  sections in the main page

--- src/app/(main-root)/page.tsx
+++ src/app/(main-root)/page.tsx
@@ -2,19 +2,29 @@ import MainPageHeader from "../ui/shared-ui/pages-components/main-page-header";
 import CommonQuestionsSection from "../ui/main-page/common-questions-section/common-questions-section";
 import InstituteInfoContainer from "../ui/main-page/institute-info-section/institute-info-container";
 import LandingImage from "../ui/main-page/landing-image";
-
 import BriefAboutUs from "../ui/main-page/brief-about-us";
+import AnimatedSection from "../ui/shared-ui/pages-components/animated-section";
 
 export default function Home() {
   return (
     <section className=" bg-slate-100">
       <MainPageHeader text="الصفحة الرئيسية" />
-      <article className="w-full mx-auto relative z-0 ">
-        <LandingImage />
-      </article>
-      <BriefAboutUs />
-      <InstituteInfoContainer />
-      <CommonQuestionsSection />
+      <AnimatedSection duration="duration-500">
+        <article className="w-full mx-auto relative z-0 ">
+          <LandingImage />
+        </article>
+      </AnimatedSection>
+      <AnimatedSection>
+        <BriefAboutUs />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <InstituteInfoContainer />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <CommonQuestionsSection />
+      </AnimatedSection>
     </section>
   );
 }
