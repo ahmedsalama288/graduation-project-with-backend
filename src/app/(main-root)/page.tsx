21c2022b952cd3e7c1f5229ab8d24cff8f117512
ahmedsalama288 <ahsalama288@gmail.com>
2024-03-30 21:35:16 +0200

    add slider-images component to the page it have different animation to switching images

--- src/app/(main-root)/page.tsx
+++ src/app/(main-root)/page.tsx
@@ -1,15 +1,17 @@
 import MainPageHeader from "../ui/shared-ui/pages-components/main-page-header";
 import CommonQuestionsSection from "../ui/main-page/common-questions-section/common-questions-section";
 import InstituteInfoContainer from "../ui/main-page/institute-info-section/institute-info-container";
-import LandingImage from "../ui/main-page/landing-image";
+// import LandingImage from "../ui/main-page/landing-image";
 import BriefAboutUs from "../ui/main-page/brief-about-us";
+import SliderImages from "../ui/main-page/hero-images/slider-images";
 
 export default function Home() {
   return (
     <section className=" bg-slate-100">
       <MainPageHeader text="الصفحة الرئيسية" />
       <article className="w-full mx-auto relative z-0 ">
-        <LandingImage />
+        <SliderImages />
+        {/* <LandingImage /> */}
       </article>
       <BriefAboutUs />
       <InstituteInfoContainer />
