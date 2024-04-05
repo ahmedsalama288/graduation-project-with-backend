9e36d5087c83f0d3ed79a0130d18b1478504dd33
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-05 23:31:42 +0200

    modify the component to accept className using cn

--- src/app/ui/shared-ui/pages-components/content-header.tsx
+++ src/app/ui/shared-ui/pages-components/content-header.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/app/lib/utils";
+
 interface props {
   text: string;
   className?: string;
@@ -5,8 +7,13 @@ interface props {
 
 export default function ContentHeader({ text, className }: props) {
   return (
-    <div className=" bg-slate-600 w-fit max-w-full rounded-md p-[10px] sm:p-4">
-      <h3 className={`text-base sm:text-lg font-semibold ${className} `}>{text}</h3>
+    <div
+      className={cn(
+        "bg-slate-600 w-fit max-w-full rounded-md p-[10px] sm:p-4",
+        className
+      )}
+    >
+      <h3 className={`text-base sm:text-lg font-semibold`}>{text}</h3>
     </div>
   );
 }
