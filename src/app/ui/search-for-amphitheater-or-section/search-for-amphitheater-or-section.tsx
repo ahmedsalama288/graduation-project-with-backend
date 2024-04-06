5f3b5e870a13ac6cc5d11fbbf1119348d5b85f38
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-06 06:33:30 +0200

    new component to handle input states and be the parent of the tree to the state

---
+++ src/app/ui/search-for-amphitheater-or-section/search-for-amphitheater-or-section.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import SearchBar from "./search-bar";
+import SearchResult from "./search-result";
+
+export default function SearchForAmphitheaterOrSection() {
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [isInputHasInvalidValue, setIsInputHasInvalidValue] = useState(false);
+
+  const handleSearchInputChange = (searchInput: string) => {
+    setSearchInputValue(searchInput);
+  };
+
+  const handleInvalidInputValue = (isInvalid: boolean) => {
+    setIsInputHasInvalidValue(isInvalid);
+  };
+
+  return (
+    <>
+      <div className=" px-2 sm:px-0">
+        <SearchBar
+          searchInputValue={searchInputValue}
+          onSearchInputChange={handleSearchInputChange}
+          isInputHasInvalidValue={isInputHasInvalidValue}
+        />
+      </div>
+      <SearchResult
+        checkInvalidInputValue={handleInvalidInputValue}
+        search={searchInputValue}
+      />
+    </>
+  );
+}
