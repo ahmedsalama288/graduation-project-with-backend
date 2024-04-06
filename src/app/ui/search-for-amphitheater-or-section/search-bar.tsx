0d5d4a3f7485e74d1737cd3cf7ee11f3f8236cae
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-06 22:26:18 +0200

    prevent the form to submit the form data

--- src/app/ui/search-for-amphitheater-or-section/search-bar.tsx
+++ src/app/ui/search-for-amphitheater-or-section/search-bar.tsx
@@ -15,7 +15,10 @@ export default function SearchBar({
   return (
     <div className=" container mx-auto mt-6">
       <div>
-        <form className=" w-full flex bg-slate-400 gap-1 rounded-md p-2">
+        <form
+          className=" w-full flex bg-slate-400 gap-1 rounded-md p-2"
+          onSubmit={(e) => e.preventDefault()}
+        >
           <button className=" p-2 rounded-md bg-slate-300" type="button">
             <MagnifyingGlassIcon className="h-6 w-6  text-slate-500" />
           </button>
@@ -37,7 +40,9 @@ export default function SearchBar({
           />
         </form>
         {isInputHasInvalidValue && (
-          <p className=" text-red-800 mt-1 text-center">لا توجد نتيجه تأكد من الاسم</p>
+          <p className=" text-red-800 mt-1 text-center">
+            لا توجد نتيجه تأكد من الاسم
+          </p>
         )}
       </div>
     </div>
