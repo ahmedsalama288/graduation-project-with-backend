24b11ea5e049dacaf73eb66f2ec53b29a73702c0
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-06 06:28:25 +0200

    update the page and add new component to it

--- src/app/(main-navbar-links)/student-services/search-for-amphitheater-or-section/page.tsx
+++ src/app/(main-navbar-links)/student-services/search-for-amphitheater-or-section/page.tsx
@@ -1,33 +1,11 @@
-import SearchBar from "@/app/ui/search-for-amphitheater-or-section/search-bar";
-import SearchResult from "@/app/ui/search-for-amphitheater-or-section/search-result";
 import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
-import { Suspense } from "react";
+import SearchForAmphitheaterOrSection from "@/app/ui/search-for-amphitheater-or-section/search-for-amphitheater-or-section";
 
-interface props {
-  searchParams?: {
-    search?: string;
-  };
-}
-
-export default function Page({ searchParams }: props) {
-  const search = searchParams?.search;
-
+export default function Page() {
   return (
     <section>
       <MainPageHeader text="الهيكل الداخلي للمبني" />
-      <div className=" px-2 sm:px-0">
-        <SearchBar />
-      </div>
-      <Suspense
-        key={search}
-        fallback={
-          <div className=" bg-slate-600">
-            <p>loading</p>
-          </div>
-        }
-      >
-        <SearchResult search={search} />
-      </Suspense>
+      <SearchForAmphitheaterOrSection />
     </section>
   );
 }
