2736d7d963337181573ccf3e74ccb630ddc4533c
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-29 17:36:40 +0300

    add Student complaints page

--- src/app/(main-navbar-links)/student-services/complaints/page.tsx
+++ src/app/(main-navbar-links)/student-services/complaints/page.tsx
@@ -1,3 +1,16 @@
-export default function Page(){
-  return <h1>شكاوي الطلاب</h1>
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
+import StudentComplaintsForm from "@/app/ui/student-services/student-complaints/student-complaints-form";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="شكاوي الطلاب" />
+      <div className=" px-2 sm:px-0">
+        <PageContentContainer>
+          <StudentComplaintsForm />
+        </PageContentContainer>
+      </div>
+    </section>
+  );
 }
