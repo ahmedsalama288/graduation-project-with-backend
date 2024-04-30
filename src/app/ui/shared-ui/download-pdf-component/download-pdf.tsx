821754d0b3391a8f4a46b744762dc433ccdc1ace
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-30 20:47:00 +0300

    add new component to display a download pdf files

---
+++ src/app/ui/shared-ui/download-pdf-component/download-pdf.tsx
@@ -0,0 +1,36 @@
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
+
+interface Props {
+  pdfName: string;
+  pdfHref: string;
+}
+
+export default function DownloadPdf({ pdfName, pdfHref }: Props) {
+  return (
+    <li
+      className="
+        transition-transform duration-300
+        translate-y-0 hover:-translate-y-1
+        max-w-full
+      "
+    >
+      <div className="rounded-md bg-slate-600 p-5 max-w-full">
+        <a
+          className="
+              flex flex-col justify-center items-center
+              p-4 h-[170px] w-[170px] max-w-full
+              border border-inherit
+              border-solid rounded-full text-base
+            "
+          href={pdfHref}
+          download={`${pdfName}.pdf`}
+        >
+          <span className="flex flex-col gap-2 justify-center items-center">
+            <DocumentArrowDownIcon className=" h-10 w-10" />
+            <span className=" font-bold">{pdfName}</span>
+          </span>
+        </a>
+      </div>
+    </li>
+  );
+}
