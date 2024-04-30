ab17878c6890e6aac21b939d5d2ddf461e8ed726
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-30 20:55:19 +0300

    add DownloadPdf component

--- src/app/(main-navbar-links)/institute-sectors/student-affairs/important-rules-for-students/page.tsx
+++ src/app/(main-navbar-links)/institute-sectors/student-affairs/important-rules-for-students/page.tsx
@@ -1,3 +1,4 @@
+import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
 import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
 import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
 import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
@@ -9,42 +10,20 @@ export default function Page() {
       <MainPageHeader text="قواعد هامه للطلاب" />
       <PageContentContainer>
         <ContentWrapper>
-          <div className=" p-4">
-            <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center">
-              <li className="flex flex-col justify-center items-center p-4 h-[170px] w-[170px] border border-inherit border-solid rounded-full">
-                <a
-                  className=" flex flex-col gap-2 justify-center items-center"
-                  href="/pdf/important-rules-for-students/student-guide.pdf"
-                  download="student-guide.pdf"
-                >
-                  <DocumentArrowDownIcon className=" h-10 w-10" />
-                  <span className=" font-bold">دليل الطالب</span>
-                </a>
-              </li>
-
-              <li className="flex flex-col justify-center items-center p-4 h-[170px] w-[170px] border border-inherit border-solid rounded-full">
-                <a
-                  className=" flex flex-col gap-2 justify-center items-center"
-                  href="/pdf/important-rules-for-students/academic-advising.pdf"
-                  download="academic-advising.pdf"
-                >
-                  <DocumentArrowDownIcon className=" h-10 w-10" />
-                  <span className=" font-bold">الإرشاد الأكاديمي</span>
-                </a>
-              </li>
-
-              <li className="flex flex-col justify-center items-center p-4 h-[170px] w-[170px] border border-inherit border-solid rounded-full">
-                <a
-                  className=" flex flex-col gap-2 justify-center items-center"
-                  href="/pdf/important-rules-for-students/scientific-leadership-guide.pdf"
-                  download="scientific-leadership-guide.pdf"
-                >
-                  <DocumentArrowDownIcon className=" h-10 w-10" />
-                  <span className=" font-bold">دليل ريادة علمية</span>
-                </a>
-              </li>
+            <ul className=" flex flex-col gap-5 p-[1px] sm:p-2 md:flex-row flex-wrap justify-around items-center">
+              <DownloadPdf
+                pdfHref="/pdf/important-rules-for-students/student-guide.pdf"
+                pdfName="دليل الطالب"
+              />
+              <DownloadPdf
+                pdfHref="/pdf/important-rules-for-students/academic-advising.pdf"
+                pdfName="الإرشاد الأكاديمي"
+              />
+              <DownloadPdf
+                pdfName="دليل ريادة علمية"
+                pdfHref="/pdf/important-rules-for-students/scientific-leadership-guide.pdf"
+              />
             </ul>
-          </div>
         </ContentWrapper>
       </PageContentContainer>
     </section>
