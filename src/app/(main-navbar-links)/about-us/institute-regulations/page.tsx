5a59774266a4b81eccf3d668434122d1291a6105
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-30 21:15:16 +0300

    add new-internal-regulation pdf

---
+++ src/app/(main-navbar-links)/about-us/institute-regulations/page.tsx
@@ -0,0 +1,29 @@
+import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
+import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
+import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="اللائحة" />
+      <PageContentContainer>
+        <ContentWrapper>
+          <div className=" p-4 min-h-[400px] flex justify-center items-center">
+            <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center w-full">
+              <DownloadPdf
+                pdfHref="/pdf/new-internal-regulation.pdf"
+                pdfName="اللائحة الجديدة"
+              />
+              <DownloadPdf
+                pdfHref="/pdf/internal-regulation.pdf"
+                pdfName="اللائحة القديمة"
+              />
+            </ul>
+          </div>
+        </ContentWrapper>
+      </PageContentContainer>
+    </section>
+  );
+}
