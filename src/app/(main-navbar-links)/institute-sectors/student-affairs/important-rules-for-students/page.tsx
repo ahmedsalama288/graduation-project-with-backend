ca5d9a9259c599d06a3a42bcdb8869b5aed78dfd
ahmedsalama288 <ahsalama288@gmail.com>
2024-04-30 21:15:30 +0300

    style modification

--- src/app/(main-navbar-links)/institute-sectors/student-affairs/important-rules-for-students/page.tsx
+++ src/app/(main-navbar-links)/institute-sectors/student-affairs/important-rules-for-students/page.tsx
@@ -2,7 +2,6 @@ import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf"
 import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
 import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
 import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
-import { DocumentArrowDownIcon } from "@heroicons/react/24/outline";
 
 export default function Page() {
   return (
@@ -10,7 +9,7 @@ export default function Page() {
       <MainPageHeader text="قواعد هامه للطلاب" />
       <PageContentContainer>
         <ContentWrapper>
-            <ul className=" flex flex-col gap-5 p-[1px] sm:p-2 md:flex-row flex-wrap justify-around items-center">
+            <ul className=" flex flex-col gap-5 px-[1px] py-4 min-h-[400px] sm:p-2 md:flex-row flex-wrap justify-around items-center">
               <DownloadPdf
                 pdfHref="/pdf/important-rules-for-students/student-guide.pdf"
                 pdfName="دليل الطالب"
