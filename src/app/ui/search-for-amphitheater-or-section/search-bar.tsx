475af16b66c772db147c8fdb434c636337af4fa8
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-03 19:15:02 +0300

    add state to handle the search input focus state and pass it to InputAutocomplete component

--- src/app/ui/search-for-amphitheater-or-section/search-bar.tsx
+++ src/app/ui/search-for-amphitheater-or-section/search-bar.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+import InputAutocomplete from "./input-autocomplete";
+import SearchInput from "./search-input";
 
 interface Props {
   searchInputValue: string;
@@ -12,33 +14,35 @@ export default function SearchBar({
   onSearchInputChange,
   isInputHasInvalidValue,
 }: Props) {
+  const [isInputInFocus, setIsInputInFocus] = useState(false);
+
+  const handleInputFocus = () => {
+    setIsInputInFocus(true);
+  };
+  const handleInputOutOfFocus = () => {
+    setTimeout(() => setIsInputInFocus(false), 250);
+  };
+
   return (
     <div className=" container mx-auto mt-6">
       <div>
         <form
-          className=" w-full flex bg-slate-400 gap-1 rounded-md p-2"
+          className=" w-full bg-slate-400 rounded-md p-2 relative"
           onSubmit={(e) => e.preventDefault()}
         >
-          <button className=" p-2 rounded-md bg-slate-300" type="button">
-            <MagnifyingGlassIcon className="h-6 w-6  text-slate-500" />
-          </button>
-          <label htmlFor="بحث عن مدرج او سكشن"></label>
-          <input
-            onChange={(e) => onSearchInputChange(e.target.value)}
-            className="
-                transition-all ease-in-out duration-100
-                w-full bg-slate-400 text-lg sm:text-xl
-                outline-2 outline-transparent
-                focus-visible:outline focus-visible:outline-slate-500
-                rounded pr-3 
-                placeholder:text-lg placeholder:text-slate-100 
-              "
-            type="text"
-            name="search"
-            placeholder="اسم المدرج او السكشن ..."
-            value={searchInputValue}
-            autoComplete="off"
-          />
+          <div>
+            <SearchInput
+              onInputFocus={handleInputFocus}
+              onInputBlur={handleInputOutOfFocus}
+              searchInputValue={searchInputValue}
+              onSearchInputChange={onSearchInputChange}
+            />
+            <InputAutocomplete
+              isInputInFocus={isInputInFocus}
+              changeInputValue={onSearchInputChange}
+              searchValue={searchInputValue}
+            />
+          </div>
         </form>
         {isInputHasInvalidValue && (
           <p className=" text-red-800 mt-1 text-center">
