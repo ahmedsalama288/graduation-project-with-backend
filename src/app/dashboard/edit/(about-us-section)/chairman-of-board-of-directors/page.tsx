a1f1e776be1396a5d3f6d4362385fa9fc54c9c5e
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-05 13:53:44 +0300

    edit page

---
+++ src/app/dashboard/edit/(about-us-section)/chairman-of-board-of-directors/page.tsx
@@ -0,0 +1,9 @@
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="تعديل صفحة رئيس مجلس الإدارة" />
+    </section>
+  )
+}
---
+++ src/app/dashboard/edit/main-page/page.tsx
@@ -0,0 +1,10 @@
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="تعديل الصفحة الرئيسية" />
+      <div></div>
+    </section>
+  )
+}
