a53ab1a89bae3e591c7c14ca3951bb6c5dc61a67
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-05 13:49:45 +0300

    add dashboard main page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import MainPageHeader from "../ui/shared-ui/pages-components/main-page-header";
+
+export default function page() {
+  return (
+    <section className=" flex-1">
+      <MainPageHeader text="Dashboard" />
+    </section>
+  )
+}
