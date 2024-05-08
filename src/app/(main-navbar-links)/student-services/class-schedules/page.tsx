ce9723535cf2352a9b323d9a0e311c8c41a97351
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-08 09:11:18 +0300

    add class-schedules page

---
+++ src/app/(main-navbar-links)/student-services/class-schedules/page.tsx
@@ -0,0 +1,24 @@
+import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
+import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="الجداول الدراسية" />
+      <PageContentContainer>
+        <ContentWrapper>
+          <div className=" p-4 min-h-[400px] flex justify-center items-center">
+            <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center w-full">
+              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الأولى" />
+              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الثانية" />
+              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الثالثة" />
+              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الرابعة" />
+            </ul>
+          </div>
+        </ContentWrapper>
+      </PageContentContainer>
+    </section>
+  );
+}
