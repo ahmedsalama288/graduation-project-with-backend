4f65e511395b6ebf9a3d97227620582cd63b0939
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-08 09:12:06 +0300

    add student-exam-schedules page

---
+++ src/app/(main-navbar-links)/student-services/exam-schedules/page.tsx
@@ -0,0 +1,24 @@
+import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
+import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
+import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
+import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
+
+export default function Page() {
+  return (
+    <section>
+      <MainPageHeader text="الجداول الإمتحانات" />
+      <PageContentContainer>
+        <ContentWrapper>
+          <div className=" p-4 min-h-[400px] flex justify-center items-center">
+            <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center w-full">
+              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-one.pdf" pdfName="جدول الفرقة الأولى" />
+              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-two.pdf" pdfName="جدول الفرقة الثانية" />
+              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-three.pdf" pdfName="جدول الفرقة الثالثة" />
+              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-four.pdf" pdfName="جدول الفرقة الرابعة" />
+            </ul>
+          </div>
+        </ContentWrapper>
+      </PageContentContainer>
+    </section>
+  );
+}
