c414dd7c5ce57d841b1cda1cd51b4986177fe772
ahmedsalama288 <ahsalama288@gmail.com>
2024-05-24 16:40:25 +0300

    improve the component by make a use new component NestedLink to handle the rendering of nested links

--- src/app/ui/root-layout/nav-bar/desktop-navbar/nav-link.tsx
+++ src/app/ui/root-layout/nav-bar/desktop-navbar/nav-link.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import {
   ChevronDownIcon,
-  ChevronLeftIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { getFirstSegment } from "@/app/lib/utils";
+import { cn, getFirstSegment } from "@/app/lib/utils";
 import { ignoredLinks } from "@/app/lib/constants";
 import { NavbarNestedLinks } from "@/app/lib/definitions";
+import NestedLink from "./nested-link";
 
 interface props {
   href: string;
@@ -16,96 +16,24 @@ interface props {
 }
 
 export default function NavLink({ href, name, nestedLinks, pathname }: props) {
-  const hrefAfterFilteringIgnoredLinks = ignoredLinks.find(
-    (link) => link === href
-  )
+  const hrefAfterFilteringIgnoredLinks = ignoredLinks.includes(href)
     ? ""
     : href;
-
   const parentSegmentInUrl = getFirstSegment(pathname);
-
   const hasNestedLinks = !!nestedLinks.length;
-  // li have a list of links
-  // and every link inside of the links
-  // have a nested links inside of it.
-
-  const renderedNestedLinks =
-    hasNestedLinks &&
-    nestedLinks.map(
-      (
-        { name, href = "", nestedLinks},
-        index
-      ) => (
-        <li
-          key={index}
-          className={`
-          w-full whitespace-nowrap text-black-gray
-          relative inline-block
-      `}
-        >
-          <Link
-            href={href || ""}
-            className={`
-              ${href == pathname ? "bg-slate-300 pr-5" : ""}
-              transition-all
-              py-3 px-4 rounded-md
-              hover:pr-5 hover:bg-slate-300
-              w-full flex justify-between items-center
-              peer
-          `}
-          >
-            {name}{" "}
-            {!!nestedLinks?.length && (
-              <ChevronLeftIcon className=" h-5 w-5 text-gray-500" />
-            )}
-          </Link>
-
-          {!!nestedLinks?.length && (
-            <ul
-              className={`
-              w-56 max-w-72 min-w-fit
-              peer hidden peer-hover:block hover:block
-              top-0 -left-full
-              absolute bg-slate-200 rounded
-              drop-shadow-lg
-          `}
-            >
-              {nestedLinks.map(({ name, href }) => (
-                <li key={href}>
-                  <Link
-                    href={href}
-                    className={` 
-                    ${href == pathname ? "bg-slate-300 pr-5" : ""}
-                    transition-all
-                    py-3 px-4 rounded-md
-                    hover:pr-5 hover:bg-slate-300
-                    w-full inline-block
-                  `}
-                  >
-                    {name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          )}
-        </li>
-      )
-    );
 
   return (
     <li
-      className={` 
-      before:content-[""] before:w-full before:h-7
-      before:absolute before:top-9 
-      before:hidden hover:before:block
-
-      whitespace-nowrap 
-      relative px-4 py-3 rounded-md cursor-pointer
-      transition-colors hover:bg-sky-700
-      group
-
-      ${href === parentSegmentInUrl ? "bg-sky-700" : ""}
-    `}
+      className={cn(
+        `
+          before:content-[""] before:w-full before:h-7
+          before:absolute before:top-9 
+          before:hidden hover:before:block
+          whitespace-nowrap relative px-4 py-3 rounded-md cursor-pointer
+          transition-colors hover:bg-sky-700 group
+        `,
+        href === parentSegmentInUrl && "bg-sky-700"
+      )}
     >
       <Link
         href={hrefAfterFilteringIgnoredLinks}
@@ -119,15 +47,25 @@ export default function NavLink({ href, name, nestedLinks, pathname }: props) {
           </>
         )}
       </Link>
-      <ul
-        className={`
-          w-56 max-w-72 min-w-fit absolute top-16 right-2 bg-slate-200 rounded
-          hidden group-hover:flex flex-col 
-          drop-shadow-lg z-[1000]
-        `}
-      >
-        {renderedNestedLinks}
-      </ul>
+      {hasNestedLinks && (
+        <ul
+          className="
+            absolute top-16 right-2 rounded bg-slate-200 
+            hidden group-hover:flex flex-col
+            w-56 max-w-72 min-w-fit
+            drop-shadow-lg z-[1000]
+          "
+        >
+          {nestedLinks.map(({ name, href = "", nestedLinks }) => (
+            <NestedLink
+              key={name}
+              name={name}
+              href={href}
+              nestedLinks={nestedLinks}
+            />
+          ))}
+        </ul>
+      )}
     </li>
   );
 }
