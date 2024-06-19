95be9ef8adfcfdccee801f8a7f0456cc81c79032
ahmedsalama288 <ahsalama288@gmail.com>
2024-06-19 12:44:29 +0300

    add string type for imageSrc prop

--- src/app/ui/shared-ui/pages-components/member-photo.tsx
+++ src/app/ui/shared-ui/pages-components/member-photo.tsx
@@ -2,7 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
 interface props {
-  imageSrc: StaticImageData;
+  imageSrc: StaticImageData | string;
   alt: string;
   href?: string;
 }
