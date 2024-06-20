70e1850f2ffda16ddc83bdbfd922170080b55855
ahmedsalama288 <ahsalama288@gmail.com>
2024-06-20 21:50:49 +0300

    fetch the pdf files from the backend

--- src/app/(main-navbar-links)/student-services/class-schedules/page.tsx
+++ src/app/(main-navbar-links)/student-services/class-schedules/page.tsx
@@ -1,9 +1,12 @@
+import { fetchClassSchedules } from "@/app/lib/data";
 import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
 import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
 import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
 import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
 
-export default function Page() {
+export default async function Page() {
+  const studentClassSchedules = await fetchClassSchedules();
+
   return (
     <section>
       <MainPageHeader text="الجداول الدراسية" />
@@ -11,10 +14,22 @@ export default function Page() {
         <ContentWrapper>
           <div className=" p-4 min-h-[400px] flex justify-center items-center">
             <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center w-full">
-              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الأولى" />
-              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الثانية" />
-              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الثالثة" />
-              <DownloadPdf pdfHref="" pdfName="جدول الفرقة الرابعة" />
+              <DownloadPdf
+                pdfHref={studentClassSchedules.firstPdfSchedule}
+                pdfName="جدول الفرقة الأولى"
+              />
+              <DownloadPdf
+                pdfHref={studentClassSchedules.secondPdfSchedule}
+                pdfName="جدول الفرقة الثانية"
+              />
+              <DownloadPdf
+                pdfHref={studentClassSchedules.theadPdfSchedule}
+                pdfName="جدول الفرقة الثالثة"
+              />
+              <DownloadPdf
+                pdfHref={studentClassSchedules.firstPdfSchedule}
+                pdfName="جدول الفرقة الرابعة"
+              />
             </ul>
           </div>
         </ContentWrapper>
--- src/app/(main-navbar-links)/student-services/exam-schedules/page.tsx
+++ src/app/(main-navbar-links)/student-services/exam-schedules/page.tsx
@@ -1,9 +1,12 @@
+import { fetchExamSchedules } from "@/app/lib/data";
 import DownloadPdf from "@/app/ui/shared-ui/download-pdf-component/download-pdf";
 import ContentWrapper from "@/app/ui/shared-ui/pages-components/content-wrapper";
 import MainPageHeader from "@/app/ui/shared-ui/pages-components/main-page-header";
 import PageContentContainer from "@/app/ui/shared-ui/pages-components/page-content-container";
 
-export default function Page() {
+export default async function Page() {
+  const examSchedules = await fetchExamSchedules();
+
   return (
     <section>
       <MainPageHeader text="جداول الإمتحانات" />
@@ -11,10 +14,22 @@ export default function Page() {
         <ContentWrapper>
           <div className=" p-4 min-h-[400px] flex justify-center items-center">
             <ul className=" flex flex-col gap-5 md:flex-row flex-wrap justify-around items-center w-full">
-              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-one.pdf" pdfName="جدول الفرقة الأولى" />
-              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-two.pdf" pdfName="جدول الفرقة الثانية" />
-              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-three.pdf" pdfName="جدول الفرقة الثالثة" />
-              <DownloadPdf pdfHref="/pdf/exam-schedule/exam-schedule-four.pdf" pdfName="جدول الفرقة الرابعة" />
+              <DownloadPdf
+                pdfHref={examSchedules.firstPdfSchedule}
+                pdfName="جدول الفرقة الأولى"
+              />
+              <DownloadPdf
+                pdfHref={examSchedules.secondPdfSchedule}
+                pdfName="جدول الفرقة الثانية"
+              />
+              <DownloadPdf
+                pdfHref={examSchedules.theadPdfSchedule}
+                pdfName="جدول الفرقة الثالثة"
+              />
+              <DownloadPdf
+                pdfHref={examSchedules.fourthPdfSchedule}
+                pdfName="جدول الفرقة الرابعة"
+              />
             </ul>
           </div>
         </ContentWrapper>
