11a5c6d831427f0ffe862392dbe479fe016802d4
ahmedsalama288 <ahsalama288@gmail.com>
2024-07-03 21:31:24 +0300

    improve component code and accessibility

--- src/app/ui/root-layout/nav-bar/desktop-navbar/nested-link.tsx
+++ src/app/ui/root-layout/nav-bar/desktop-navbar/nested-link.tsx
@@ -16,40 +16,57 @@ export default function NestedLink({
   nestedLinks,
 }: NestedLinkProps) {
   const pathname = usePathname();
+  const hasNestedLinks = !!nestedLinks?.length;
 
   return (
     <li className="relative inline-block w-full whitespace-nowrap text-black-gray">
-      <Link
-        href={href || ""}
-        className={`
-        ${href === pathname ? "bg-slate-300 pr-5" : ""}
-        transition-all py-3 px-4 rounded-md hover:pr-5 hover:bg-slate-300
-        w-full flex justify-between items-center peer
-      `}
-      >
-        {name}
-        {!!nestedLinks?.length && (
-          <ChevronLeftIcon className="h-5 w-5 text-gray-500" />
-        )}
-      </Link>
-      {!!nestedLinks?.length && (
-        <ul className="absolute bg-slate-200 rounded drop-shadow-lg top-0 -left-56 w-56 min-w-fit hidden peer-hover:block hover:block">
-          {nestedLinks.map(({ name, href = "" }) => (
-            <li key={href}>
-              <Link
-                href={href}
-                className={cn(
-                  `transition-all py-3 px-4 rounded-md
+      {!hasNestedLinks && (
+        <Link
+          href={href}
+          className={`
+            ${href === pathname ? "bg-slate-300 pr-5" : ""}
+            transition-all py-3 px-4 rounded-md hover:pr-5 hover:bg-slate-300
+            w-full flex justify-between items-center peer
+          `}
+        >
+          {name}
+        </Link>
+      )}
+
+      {hasNestedLinks && (
+        <>
+          <button
+            type="button"
+            className={`
+              ${href === pathname ? "bg-slate-300 pr-5" : ""}
+              transition-all py-3 px-4 rounded-md hover:pr-5 hover:bg-slate-300
+              w-full flex justify-between items-center peer
+            `}
+          >
+            {name}
+            {!!nestedLinks?.length && (
+              <ChevronLeftIcon className="h-5 w-5 text-gray-500" />
+            )}
+          </button>
+
+          <ul className="absolute bg-slate-200 rounded drop-shadow-lg top-0 -left-64 w-64 min-w-fit hidden peer-hover:block hover:block">
+            {nestedLinks.map(({ name, href = "" }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={cn(
+                    `transition-all py-3 px-4 rounded-md
                   hover:pr-5 hover:bg-slate-300 w-full
                   inline-block`,
-                  href === pathname && "bg-slate-300 pr-5"
-                )}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+                    href === pathname && "bg-slate-300 pr-5"
+                  )}
+                >
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </li>
   );
