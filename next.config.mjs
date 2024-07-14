c9a94f26f78fdb3082b4c137af9a5dff2ca29a2a
ahmedsalama288 <ahsalama288@gmail.com>
2024-07-14 12:19:35 +0300

    update pwa configurations

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfigFunction = async (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
     const withPWA = (await import("@ducanh2912/next-pwa")).default({
       dest: "public",
-      cacheOnFrontEndNav: true,
+      cacheOnFrontEndNav: false,
       cacheStartUrl: true,
       reloadOnOnline: true,
       disable: false,
