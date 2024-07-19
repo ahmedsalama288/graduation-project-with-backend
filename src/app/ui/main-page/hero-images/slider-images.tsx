3bfd48873b96db35691188d29f07802e1d303369
ahmedsalama288 <ahsalama288@gmail.com>
2024-07-19 18:44:43 +0300

    make the hero image not animated

--- src/app/ui/main-page/hero-images/slider-images.tsx
+++ src/app/ui/main-page/hero-images/slider-images.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Image from "next/image";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
-import AnimatedSection from "../../shared-ui/pages-components/animated-section";
 
 import imageOne from "@/../../public/landing-images/landing-one.webp";
 import imageTwo from "@/../../public/landing-images/landing-two.webp";
@@ -49,6 +48,7 @@ export default function SliderImages() {
           alt="landing page image"
           src={imageSrc}
           placeholder="blur"
+          priority={false}
           quality={85}
           fill
           sizes="(max-width: 480px) 200vw, 100vw"
@@ -61,49 +61,45 @@ export default function SliderImages() {
   ));
 
   return (
-    <AnimatedSection duration="duration-500">
-      <figure className="relative overflow-hidden w-full hero-img-hight">
-        <div className="absolute inset-0 bg-black/20 z-20" />
-        <div className="overflow-hidden">
-          <div
-            className="flex flex-row-reverse transition-transform duration-500 ease-in-out transform"
-            style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
-          >
-            {slides}
-          </div>
-          <>
-            <button
-              onClick={goToPreviousImageHandler}
-              className="text-white absolute top-1/2 -translate-y-1/2 left-1 z-50 cursor-pointer"
-              aria-label="view previous image"
-            >
-              <ChevronLeftIcon className="h-[45px] w-[45px]  md:h-14 md:w-14" />
-            </button>
-
-            <button
-              className="text-white absolute top-1/2 -translate-y-1/2 right-1 z-50 cursor-pointer"
-              onClick={goToNextImageHandler}
-              aria-label="view next image"
-            >
-              <ChevronRightIcon className="h-[45px] w-[45px]  md:h-14 md:w-14" />
-            </button>
-          </>
+    <figure className="relative overflow-hidden w-full hero-img-hight">
+      <div className="absolute inset-0 bg-black/20 z-20" />
+      <div className="overflow-hidden">
+        <div
+          className="flex flex-row-reverse transition-transform duration-500 ease-in-out transform"
+          style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
+        >
+          {slides}
         </div>
+        <>
+          <button
+            onClick={goToPreviousImageHandler}
+            className="text-white absolute top-1/2 -translate-y-1/2 left-1 z-50 cursor-pointer"
+            aria-label="view previous image"
+          >
+            <ChevronLeftIcon className="h-[45px] w-[45px]  md:h-14 md:w-14" />
+          </button>
 
-        <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30">
-          <AnimatedSection>
-            <figcaption
-              className="
+          <button
+            className="text-white absolute top-1/2 -translate-y-1/2 right-1 z-50 cursor-pointer"
+            onClick={goToNextImageHandler}
+            aria-label="view next image"
+          >
+            <ChevronRightIcon className="h-[45px] w-[45px]  md:h-14 md:w-14" />
+          </button>
+        </>
+      </div>
+
+      <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30">
+        <figcaption
+          className="
                   flex flex-col justify-center items-center
                   text-3xl md:text-5xl whitespace-nowrap !leading-[1.2]
                 "
-            >
-              <span>معهد الدلتا العالي</span>
-              <span>للنظم والمعلومات الإدارية</span>
-            </figcaption>
-          </AnimatedSection>
-        </div>
-      </figure>
-    </AnimatedSection>
+        >
+          <span>معهد الدلتا العالي</span>
+          <span>للنظم والمعلومات الإدارية</span>
+        </figcaption>
+      </div>
+    </figure>
   );
 }
