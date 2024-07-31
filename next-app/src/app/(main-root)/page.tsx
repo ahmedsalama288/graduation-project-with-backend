3038a8598e9b0c41404f46a669cb4597335aa2d5
ahmedsalama288 <ahsalama288@gmail.com>
2024-07-31 16:42:41 +0300

    refactor: fetch the main-page data and pass it to its components

--- next-app/src/app/(main-root)/page.tsx
+++ next-app/src/app/(main-root)/page.tsx
@@ -1,21 +1,36 @@
 import MainPageHeader from "../ui/shared-ui/pages-components/main-page-header";
 import CommonQuestionsSection from "../ui/main-page/common-questions-section/common-questions-section";
 import InstituteInfoContainer from "../ui/main-page/institute-info-section/institute-info-container";
-// import LandingImage from "../ui/main-page/landing-image";
 import BriefAboutUs from "../ui/main-page/brief-about-us";
 import SliderImages from "../ui/main-page/hero-images/slider-images";
+import { apiUrl } from "../lib/constants";
+import { getPageByName } from "../lib/data";
+import { MainPageData } from "../lib/definitions";
 
-export default function Home() {
+export default async function Home() {
+  const mainPageData: MainPageData = await getPageByName("main-page");
+
+  const {
+    briefAboutUsSection,
+    studentsNumbersSection,
+    commonQuestionsSection,
+  } = mainPageData;
+
   return (
     <section className=" bg-slate-100">
       <MainPageHeader text="الصفحة الرئيسية" />
       <article className="w-full mx-auto relative z-0 ">
         <SliderImages />
-        {/* <LandingImage /> */}
       </article>
-      <BriefAboutUs />
-      <InstituteInfoContainer />
-      <CommonQuestionsSection />
+      <BriefAboutUs
+        paragraph={briefAboutUsSection.paragraph}
+        instituteGoals={briefAboutUsSection.instituteGoals}
+      />
+      <InstituteInfoContainer
+        currentStudents={studentsNumbersSection.currentStudents}
+        graduatedStudents={studentsNumbersSection.graduatedStudents}
+      />
+      <CommonQuestionsSection commonQuestionsList={commonQuestionsSection}/>
     </section>
   );
 }
