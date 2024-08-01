027ae53ff8d87cef06a06b876c0863bb1525a2df
ahmedsalama288 <ahsalama288@gmail.com>
2024-08-01 15:28:24 +0300

    Modify component style

--- next-app/src/app/ui/shared-ui/pages-components/page-content-container.tsx
+++ next-app/src/app/ui/shared-ui/pages-components/page-content-container.tsx
@@ -11,7 +11,7 @@ export default function PageContentContainer({
     <div className=" p-2">
       <div
         className={cn(
-          "container mx-auto p-4 sm:p-8 bg-slate-400 mt-8 rounded-lg",
+          "container mx-auto p-4 sm:p-8 bg-slate-400 mt-8 rounded-lg flex flex-col gap-5 sm:gap-6",
           className
         )}
       >
